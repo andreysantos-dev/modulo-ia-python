273abb0c77b65b887711c8483684ecb921430f2e
andreysantos-dev <andreydasilvasantos3@gmail.com>
2025-09-11 16:27:01 -0300

    Concluido 3 exercicios

---
+++ atividade-pratica1/calculadora_preco_total.py
@@ -0,0 +1,12 @@
+# Este programa calcula o preço total de uma compra
+
+# Solicita informações sobre o produto ao usuário
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+# Calcula o preço total
+preco_total = preco_unitario * quantidade
+
+# Exibe o resultado
+print(preco_total)
--- atividade-pratica1/hello_world.py
+++ atividade-pratica1/hello_world.py
@@ -1,1 +1,5 @@
+# Este programa imprime um comentário de saudação.
+
+# Imprime a mensagem na tela.
+
 print("Hello, World!")
