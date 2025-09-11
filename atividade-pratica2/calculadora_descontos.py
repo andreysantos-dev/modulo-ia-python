899544c1c5510c6e1ec41be8a5da9ec5b7a22a05
andreysantos-dev <andreydasilvasantos3@gmail.com>
2025-09-11 17:23:13 -0300

    Calculadora de Descontos

---
+++ atividade-pratica2/calculadora_descontos.py
@@ -0,0 +1,17 @@
+# Calculadora de desconto em uma loja
+
+# Informações do produto
+nome_produto = input("Digite o nome do produto: ")
+preco_original = float(input("Digite o valor do produto: "))
+porcentagem_desconto = int(input("Digite a porcentagem de desconto: "))
+
+# Cálculo do desconto e preço final
+valor_desconto = preco_original * (porcentagem_desconto / 100)
+preco_final = preco_original - valor_desconto
+
+# Exibição dos resultados
+print(f"Produto: {nome_produto}")
+print(f"Preço: R$ {preco_original}")
+print(f"Desconto: {porcentagem_desconto}%")
+print(f"Valor Desconto: R$ {valor_desconto}")
+print(f"Preço Final: R$ {preco_final}")
